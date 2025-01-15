03049157f2df8422189ae05c40560bc83a59e71b
Yabe12 <yeabsirabehailu92@gmail.com>
2025-01-15 16:03:32 +0300

    start building add user info functinailty

--- usingC++/pcRegistration.cpp
+++ usingC++/pcRegistration.cpp
@@ -43,8 +43,10 @@ struct nonstaff{
 student *student_head = NULL, *student_tall =NULL;
 staff *staff_head = NULL, *staff_tall = NULL;
 nonstaff *nonstaff_head = NULL, *nonstaff_tall = NULL;
-
-void add_user(){
+  void add_student();
+  void add_staff();
+  void add_nonstaff();
+  void add_user();
   void add_student(){
       
   student *new_student = new student;
@@ -67,12 +69,53 @@ void add_user(){
   cin >> new_student->serial;
 
   };
-  void add_staff(){
+  void add_nonstaff(){
+    nonstaff *new_nonstaff = new nonstaff;
+    cout << "Enter nonstaff's name: ";
+    cin.ignore();
+    cin.getline(new_nonstaff->name, 20);
+    cout << "Enter nonstaff's ID: ";
+    cin >> new_nonstaff->id;
+    cout << "Enter nonstaff's gender (M/F): ";
+    cin >> new_nonstaff->gender;
+    cout << "Enter nonstaff phone: " ;
+    cin << new_nonstaff->phone;
+    cout << "Enter nonstaff computer name: ";
+    cin >> new_nonstaff->pcname;
+    cout << "Enter nonstaff computer serial number: ";
+    cin >> new_nonstaff->serial;
+    cout << "Enter nonstaff computer model: ";
+    cin >> new_nonstaff->model;
+
 
   };
-  void add_nonstaff(){
-    
+   void add_staff(){
+    staff *new_staff=new staff();
+    cout << "Enter staff's name: ";
+  cin.ignore();
+  cin.getline(new_staff->name, 20);
+  cout << "Enter staff's ID: ";
+  cin >> new_staff->id;
+  cout << "Enter staff's gender (M/F): ";
+  cin >> new_staff->gender;
+  cout << "Enter staff's role: ";
+  cin >> new_staff->role;
+  cout << "Enter staff's type of PC (laptop/desktop): ";
+  cin >> new_staff->typeofpc;
+  cout << "Enter staff's phone number: ";
+  cin >> new_staff->phone;
+  cout << "Enter staff's computer name: ";
+  cin >> new_staff->pcname;
+  cout << "Enter staff's computer serial number: ";
+  cin >> new_staff->serial;
+  cout << "Enter staff's computer model: ";
+  cin >> new_staff->model;
+
   };
+void add_user(){
+
+ 
+  
   int choice;
   while(true){
     cout << "Choose a type of user:" << endl;
