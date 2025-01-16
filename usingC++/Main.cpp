918a8ff7d6769b2367fbcd5aa51ff5dd3844cdad
Yabe12 <yeabsirabehailu92@gmail.com>
2025-01-16 21:32:23 +0300

    add manu for user update functinality

--- usingC++/Main.cpp
+++ usingC++/Main.cpp
@@ -274,3 +274,29 @@ void display(){
     }
   }
 }
+// function for update functionally
+void Update(){
+  int choice;
+  while(true){
+    cout << "Choose a type of user to update :" << endl;
+    cout << "1. student" << endl;
+    cout << "2. staff" << endl;
+    cout << "3. nonstaff" << endl;
+    cout << "4. Exit" << endl;
+    cin >> choice;
+    if(choice == 1){
+      // update_student();
+      break;
+    } else if(choice == 2){
+      // update_staff();
+      break;
+    } else if(choice == 3){
+      // update_nonstaff();
+      break;
+    } else if(choice == 4){
+      return;
+    } else{
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  }
+};
