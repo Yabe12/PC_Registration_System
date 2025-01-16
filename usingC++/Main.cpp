c14ab6429f698db655ba2158a01263b5ad85762a
Yabe12 <yeabsirabehailu92@gmail.com>
2025-01-16 21:30:58 +0300

    add manu for user display functinality

--- usingC++/Main.cpp
+++ usingC++/Main.cpp
@@ -222,7 +222,7 @@ void add_user(){
   }
 
 }
-// function for search button 
+// function for search functionaily
 void search(){
   int choice;
   while(true){
@@ -240,6 +240,32 @@ void search(){
       break;
     } else if(choice == 3){
     //   search_nonstaff();
+      break;
+    } else if(choice == 4){
+      return;
+    } else{
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  }
+}
+// function for display functinally
+void display(){
+  int choice;
+  while(true){
+    cout << "Choose a type of user to search and display:" << endl;
+    cout << "1. student" << endl;
+    cout << "2. staff" << endl;
+    cout << "3. nonstaff" << endl;
+    cout << "4. Exit" << endl;
+    cin >> choice;
+    if(choice == 1){
+      // display_student();
+      break;
+    } else if(choice == 2){
+      // display_staff();
+      break;
+    } else if(choice == 3){
+      // display_nonstaff();
       break;
     } else if(choice == 4){
       return;
