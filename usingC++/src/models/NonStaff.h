611c1517bf5b1ccbcbcd1adcd9e199e714a901ef
Fitsumhelina <permin1501424@gmail.com>
2025-01-20 22:16:31 +0300

    model update

--- usingC++/src/models/NonStaff.h
+++ usingC++/src/models/NonStaff.h
@@ -8,8 +8,6 @@ struct NonStaff {
     char name[50];
     char id[20];
     char gender;
-    char department[50];
-    char position[50];
     long long phone;
     char pcname[50];
     char serial[50];
